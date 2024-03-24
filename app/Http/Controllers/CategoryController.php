3325497170c614de8dc035f9df6bdca3a17e5210
Your Name <taido7199@gmail.com>
2024-03-24 19:47:04 +0700

    commit 1

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,20 +2,51 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\Category;
 use Illuminate\Http\Request;
+
 
 class CategoryController extends Controller
 {
+    private $htmlSelect;
+
+    public function __construct() {
+        $this->htmlSelect ='';
+    }
     public function create()
     {
-//        $data = Category:: all();
+        $data = Category:: all();
 //        foreach( $data as $value){
 //            if($value['parent_id']==0){
-//                echo "<option>". $value['name']."</option>";
+//                echo "<option>".  $value['name']."</option>";
+//
+//                foreach ($data as $value2) {
+//                    if($value2['parent_id']== $value['id']) {
+//                        echo "<option>". '-'. $value2['name']."</option>";
+//
+//                        foreach ($data as $value3) {
+//                            if($value3['parent_id']== $value2['id']) {
+//                               echo "<option>". '--'. $value3['name']."</option>";
+//                            }
+//                        }
+//                    }
+//                }
 //            }
 //        }
-        return view('category.add');
+//
+         $htmlOption = $this->categoryRecusive(id: 0);
+        return view('category.add', compact('htmlOption'));
+   }
+
+    function categoryRecusive($id, $text = '') {
+        $data = Category:: all();
+        foreach ($data as $value) {
+            if ($value['parent_id'] == $id) {
+                $this->htmlSelect .= "<option>"  .$text. $value['name'] . "</option>";
+               $this -> categoryRecusive($value['id'],  $text .'-');
+            }
+        }
+        return  $this->htmlSelect;
     }
     public function index()
     {
